43876e9357758ea72435e0aeec7ff7368a3aead0
Tetiana-Kor <63547594+Tetiana-Kor@users.noreply.github.com>
2020-10-31 19:58:43 +0200

    Додає рішення

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import './styles.css';
-import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/PNotify.css';
+
 import './js/script.js';
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,9 @@
-import API from './fetchCountries.js';
+import API from './fetchCountries';
+
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
 import countriesMarkup from '../templates/countries-markup.hbs';
 import counriesList from '../templates/countries-list.hbs';
 
@@ -6,17 +11,58 @@ const debounce = require('lodash.debounce');
 
 const refs = {
   searchInput: document.querySelector('.input-control'),
-  countriesContainer: document.querySelector('#js-card-container'),
+  countriesContainer: document.querySelector('.js-card-container'),
 };
 
-// refs.searchInput.addEventListener(
-//   'input',
-//   debounce(() => {
-//     onSearch;
-//   }, 500),
-// );
+refs.searchInput.addEventListener('input', debounce(onSearch, 500));
 
-// function onSearch() {
-//   const countrySearch = searchInput.value;
-//   console.log(countrySearch);
-// }
+function onSearch(e) {
+  let countrySearch = e.target.value;
+
+  if (!countrySearch) {
+    clearMarkup();
+    return;
+  }
+
+  API.fetchCountries(countrySearch).then(chooseCountry).catch(onError);
+}
+
+function chooseCountry(countries) {
+  if (countries.length > 10) {
+    tooManyMatchesError();
+  } else if (countries.length > 1 && countries.length <= 10) {
+    renderCountryMarkup(counriesList, countries);
+  } else if (countries.length === 1) {
+    renderCountryMarkup(countriesMarkup, countries);
+  } else {
+    noResults();
+  }
+}
+
+function renderCountryMarkup(option, countries) {
+  const markup = option(countries);
+  refs.counriesList.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearMarkup() {
+  refs.counriesList.innerHTML = '';
+}
+
+function onError(error) {
+  clearMarkup();
+  console.log(error);
+}
+
+function tooManyMatchesError() {
+  error({
+    text: 'Too many matches found. Please enter a more specific query!',
+    delay: 1500,
+  });
+}
+
+function noResults() {
+  error({
+    text: 'No such country. Please enter another query!',
+    delay: 2000,
+  });
+}
